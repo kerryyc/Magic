ab420309f64281643d357c2652e975d169571e6e
unknown <kerryc635@gmail.com>
2020-01-09 18:22:49 -0800

    changed the way multiple card faces are stored in DB

--- scripts/updateMagicDatabase.py
+++ scripts/updateMagicDatabase.py
@@ -24,7 +24,7 @@ print('Opened DB')
 
 # parse through Scryfall API sets (no all cards due to limited database storage)
 magicSets = ['thb', 'ptg', 'eld', 'm20', 'mh1', 'war', 'rna', 'uma', 'grn', 'm19', 'dom', 'a25', 'rix', 'e02', 'xln', 'hou', 'akh']
-gAttributes = ['rulings_uri', 'cmc', 'color_identity', 'legalities', 'mana_cost', 'name', 'type_line', 'prices', 'rarity', 'set_name', 'set', 'textless']
+gAttributes = ['foil', 'rulings_uri', 'cmc', 'color_identity', 'legalities', 'mana_cost', 'name', 'type_line', 'prices', 'rarity', 'set_name', 'set', 'textless']
 pAttributes = ['all_parts', 'card_faces', 'colors', 'loyalty', 'oracle_text', 'power', 'toughness', 'artist', 'flavor_text', 'image_uris']
 URI = 'https://api.scryfall.com/sets/'
 # for mSet in magicSets:
@@ -43,12 +43,31 @@ for mSet in ['eld']:
             # update card with attributes that can be null or missing from the JSON response
             for a in pAttributes:
                 if a in c:
-                    card.update({a : c[a]})
+
+                    # if the attribute is a card face, add only specific parts of that card face
+                    if a == 'card_faces':
+                        card.update({a : []})
+                        for cf in c[a]:
+                            card_face = {
+                                'name' : cf['name'],
+                                'mana_cost' : cf['mana_cost'],
+                                'type_line' : cf['type_line']
+                            }
+                            for cf_a in pAttributes:
+                                if cf_a in cf:
+                                    card_face.update({cf_a : cf[cf_a]})
+
+                            card[a].append(card_face)
+                    
+                    else:
+                        card.update({a : c[a]})
                 
             try:
                 result = db.all.insert_one(card)
+                print('%s (%s) added' % (card['name'], card['card_id']))
             except Exception as exc: 
                 errorLogFile.write('%s/%s: %s\n' % (mSet, c['id'], str(exc)))
+                print('Error adding %s from %s' % (c['id'], mSet))
 
         if not data['has_more']:
             break        
