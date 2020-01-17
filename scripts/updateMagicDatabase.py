c4a42403a0b8620b3b0e547c9278b3ee96458634
unknown <kerryc635@gmail.com>
2020-01-16 18:12:33 -0800

    Now skips cards that are already in the database. Divided up into functions and added proper docstrings

--- scripts/updateMagicDatabase.py
+++ scripts/updateMagicDatabase.py
@@ -6,73 +6,125 @@ import requests
 import os
 import json
 
-# read config file to get username and password to access MongoDB
-path = Path(os.path.dirname(os.path.abspath(__file__)))
-with open(str(path.parent) + '\\config.json') as f:
-    data = json.load(f)
-    user = data['mongodb']['user']
-    password = data['mongodb']['password']
+def getRequest(u):
+    """ 
+    Get request and sleep for 100 milliseconds 
+    
+    Args:
+        u (str): url of request
+    
+    Returns:
+        request: result of getting a request at the url
+    """
 
-# create error log file
-errorLogFile = open(str(path) + "\\errorlog.txt", "w")
-print('Created Error Log')
+    r = requests.get(url = u)
+    sleep(0.1)
+    return r
 
-# access MongoDB cluster and Magic database
-client = MongoClient("mongodb+srv://" + user + ":" + password + "@cluster0-og8a0.mongodb.net/test?retryWrites=true&w=majority")
-db = client.magic
-print('Opened DB')
+def openMongoDB(path):
+    """
+    Using the config file, open up the MongoDB database
 
-# parse through Scryfall API sets (no all cards due to limited database storage)
-magicSets = ['thb', 'ptg', 'eld', 'm20', 'mh1', 'war', 'rna', 'uma', 'grn', 'm19', 'dom', 'a25', 'rix', 'e02', 'xln', 'hou', 'akh']
-gAttributes = ['foil', 'rulings_uri', 'cmc', 'color_identity', 'legalities', 'mana_cost', 'name', 'type_line', 'prices', 'rarity', 'set_name', 'set', 'textless']
-pAttributes = ['all_parts', 'card_faces', 'colors', 'loyalty', 'oracle_text', 'power', 'toughness', 'artist', 'flavor_text', 'image_uris']
-URI = 'https://api.scryfall.com/sets/'
-# for mSet in magicSets:
-for mSet in ['eld']:
-    print('Parsing Set %s' % mSet)
-    r = requests.get(url = requests.get(url = URI + mSet).json()['search_uri'])
-    data = r.json()
-    while True:
-        for c in data['data']:
-            card = {'card_id' : c['id']}
+    Args:
+        path (Path): path to the parent directory where config.json is stored
 
-            # update card with attributes guaranteed to show up in the JSON response
-            for a in gAttributes:
-                card.update({a : c[a]})
-            
-            # update card with attributes that can be null or missing from the JSON response
-            for a in pAttributes:
-                if a in c:
+    Returns:
+        db (MongoClient.database_name): returns the main database 
+    """
+    # read config file to get username and password to access MongoDB
+    with open(str(path.parent) + '\\config.json') as f:
+        data = json.load(f)
+        user = data['mongodb']['user']
+        password = data['mongodb']['password']
 
-                    # if the attribute is a card face, add only specific parts of that card face
-                    if a == 'card_faces':
-                        card.update({a : []})
-                        for cf in c[a]:
-                            card_face = {
-                                'name' : cf['name'],
-                                'mana_cost' : cf['mana_cost'],
-                                'type_line' : cf['type_line']
-                            }
-                            for cf_a in pAttributes:
-                                if cf_a in cf:
-                                    card_face.update({cf_a : cf[cf_a]})
+    # access MongoDB cluster and Magic database
+    client = MongoClient("mongodb+srv://" + user + ":" + password + "@cluster0-og8a0.mongodb.net/test?retryWrites=true&w=majority")
+    db = client.magic
+    return db
 
-                            card[a].append(card_face)
-                    
-                    else:
-                        card.update({a : c[a]})
+def addCardsToDatabase(db, magicSets):
+    """
+    Parse through all cards in the specified Magic sets from Scryfall API and add to the database
+
+    Args:
+        db (MongoClient.db_name.collection): MongoDB collection to store cards
+        magicSets ([str]): array of Magic sets to get cards from
+
+    Attributes:
+        gAttributes ([str]): array of attributes that are guaranteed to appear in the Scryfall API response
+        pAttributes ([str]): array of attirubtes that might be null or empty in the Scryfall API response  
+    """
+
+    gAttributes = ['name', 'foil', 'rulings_uri', 'cmc', 'color_identity', 'legalities', 'type_line', 'prices', 'rarity', 'set_name', 'set', 'textless']
+    pAttributes = ['mana_cost', 'all_parts', 'card_faces', 'colors', 'loyalty', 'oracle_text', 'power', 'toughness', 'artist', 'flavor_text', 'image_uris']
+    URI = 'https://api.scryfall.com/sets/'
+
+    for mSet in magicSets:
+        print('Parsing Set %s' % mSet)
+        r = getRequest(getRequest((URI + mSet)).json()['search_uri'])
+        data = r.json()
+        while True:
+            for c in data['data']:
+                # if the card exists in the database already, skip it
+                if db.count_documents({'card_id' : c['id']}, limit = 1) != 0:
+                    print(c['name'] + ' already in DB')
+                    continue
+
+                card = {'card_id' : c['id']}
+
+                # update card with attributes guaranteed to show up in the JSON response
+                for a in gAttributes:
+                    card.update({a : c[a]})
                 
-            try:
-                result = db.all.insert_one(card)
-                print('%s (%s) added' % (card['name'], card['card_id']))
-            except Exception as exc: 
-                errorLogFile.write('%s/%s: %s\n' % (mSet, c['id'], str(exc)))
-                print('Error adding %s from %s' % (c['id'], mSet))
+                # update card with attributes that can be null or missing from the JSON response
+                for a in pAttributes:
+                    if a in c:
 
-        if not data['has_more']:
-            break        
-        sleep(0.1) # 100 millisecond delay before requesting from API
-        data = requests.get(url = data['next_page']).json()
+                        # if the attribute is a card face, add only specific parts of that card face
+                        if a == 'card_faces':
+                            card.update({a : []})
+                            for cf in c[a]:
+                                card_face = {
+                                    'name' : cf['name'],
+                                    'mana_cost' : cf['mana_cost'],
+                                    'type_line' : cf['type_line']
+                                }
+                                for cf_a in pAttributes:
+                                    if cf_a in cf:
+                                        card_face.update({cf_a : cf[cf_a]})
 
-print('Finished')
-errorLogFile.close()
+                                card[a].append(card_face)
+                        
+                        else:
+                            card.update({a : c[a]})
+                    
+                try:
+                    result = db.insert_one(card)
+                    print('%s (%s) added' % (card['name'], card['card_id']))
+                except Exception as exc: 
+                    errorLogFile.write('%s/%s: %s\n' % (mSet, c['id'], str(exc)))
+                    print('Error adding %s from %s' % (c['id'], mSet))
+
+            if not data['has_more']:
+                print()
+                break        
+            data = getRequest(data['next_page']).json()
+
+
+if __name__ == '__main__':
+    path = Path(os.path.dirname(os.path.abspath(__file__)))
+    magicSets = ['thb', 'ptg', 'eld', 'm20', 'mh1', 'war', 'rna', 'uma', 'grn', 'm19', 'dom', 'a25', 'rix', 'e02', 'xln', 'hou', 'akh']
+
+    # create error log file
+    errorLogFile = open(str(path) + "\\errorlog.txt", "w")
+    print('Created Error Log')
+
+    # open DB for all magic cards
+    db = openMongoDB(path).all # open magic.all collection
+    print('Opened DB')
+
+    # add all cards from magicSets to DB
+    addCardsToDatabase(db, magicSets)
+
+    print('Finished')
+    errorLogFile.close()
